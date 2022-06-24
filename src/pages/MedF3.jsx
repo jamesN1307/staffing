9d824a5299687610b397fd756ff67fad97b57a6f
Kika <joku.banks@gmail.com>
2022-06-23 20:45:25 -0700

    most headers done

--- src/pages/MedF3.jsx
+++ src/pages/MedF3.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const MedF3 = () => {
+  const current = new Date();
+  const today = `${current.getMonth()+1}/${current.getDate()}/${current.getFullYear()}`
   return (
-    <div>MedF3</div>
+    <div>
+      <header className='columns-2'>
+        <h1>MedF3</h1>
+        <h1>Date: {today}</h1>     
+      </header>
+    </div>
   )
 }
 
