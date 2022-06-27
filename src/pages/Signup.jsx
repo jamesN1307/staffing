ebc09fc59e803f007bc2ae2174f4c8539cfca3b5
Eric Taylor <eric.taylor1012@gmail.com>
2022-06-27 15:11:42 -0700

    signup without post

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,31 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Signup extends Component {
     constructor(props) {
         super(props);
 
         this.initialState = {
+            name: '',
+            phoneNumber: '',
             username: '',
             password: '',
-            firstName: '',
-            lastName: '',
-            phoneNumber: '',
-            notes: ''
         };
         // this.handleChange = this.handleChange.bind( this );
         this.state = this.initialState;
     }
 
     handleChange = event => {
-        const { username, password, firstName, lastName, phoneNumber, notes, value } = event.target;
+        const { name, phoneNumber, username, password, value } = event.target;
 
         this.setState({
+            [name]: value,
+            [phoneNumber]: value,
             [username]: value,
             [password]: value,
-            [firstName]: value,
-            [lastName]: value,
-            [phoneNumber]: value,
-            [notes]: value,
         });
     }
 
@@ -43,31 +39,29 @@ class Signup extends Component {
     }
 
     render() {
-        const { firstName, lastName, username, password, phoneNumber, notes } = this.state;
+        const { name, phoneNumber, username, password } = this.state;
         const current = new Date();
         const today = `${current.getMonth() + 1}/${current.getDate()}/${current.getFullYear()}`
-
         return (
             <div>
                 <header className='columns-2'>
-                    <h1>Sign Up</h1>
+                    <h1>Signup</h1>
                     <h1>Date: {today}</h1>
-                </header>
-                <br></br>
+                </header><br />
                 <form className="flex flex-col columns-2" onSubmit={this.onFormSubmit}>
-                    <label>First Name:</label>
+                    <label>Full Name:</label>
                     <input
                         type="text"
-                        name="firstName"
-                        id="firstName"
-                        value={firstName}
+                        name="name"
+                        id="name"
+                        value={name}
                         onChange={this.handleChange} />
-                    <label>Last Name:</label>
+                    <label>Phone Number:</label>
                     <input
                         type="text"
-                        name="lastName"
-                        id="lastName"
-                        value={lastName}
+                        name="phoneNumber"
+                        id="phoneNumber"
+                        value={phoneNumber}
                         onChange={this.handleChange} />
                     <label>Username:</label>
                     <input
@@ -82,20 +76,6 @@ class Signup extends Component {
                         name="password"
                         id="password"
                         value={password}
-                        onChange={this.handleChange} />
-                    <label>Phone Number:</label>
-                    <input
-                        type="text"
-                        name="phoneNumber"
-                        id="phoneNumber"
-                        value={phoneNumber}
-                        onChange={this.handleChange} />
-                    <label>Notes:</label>
-                    <input
-                        type="text"
-                        name="notes"
-                        id="notes"
-                        value={notes}
                         onChange={this.handleChange} />
                     <button type="submit">
                         Submit
